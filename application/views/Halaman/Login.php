4b67a14cfa997aa862cf55a484b814da4b1a78bf
sandrosole <128021802+sandrosole@users.noreply.github.com>
2024-06-12 17:09:10 +0700

    ganti lgin dan nambah css

--- application/views/Halaman/Login.php
+++ application/views/Halaman/Login.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" type = "text/css"
+        href="<?php echo base_url();?>aset/css/login.css">
 </head>
     <style>
         P {
@@ -18,23 +20,29 @@
         }
     </style>
 <body>
-    <h1><b>Login</b></h1>
-    <p>E-Rapot 4E</p>
-    <?php if($this->session->flashdata('message')): ?>
-        <?php echo ($this->session->flashdata('message')); ?>
-    <?php endif; ?>
-    <form action="<?= base_url('Auth'); ?>" method="post">
-        <div>
-        <label>Username:</label>
-        <input type="text" placeholder="Username" name="username" required/>
-        <?= form_error('username', '<small class="text-danger pl-3">','</small>'); ?>
+    <div class="kotak"> 
+        <div class="item"> 
+            <h1><b>Login</b></h1>
+                        <?php if($this->session->flashdata('message')): ?>
+                            <?php echo ($this->session->flashdata('message')); ?>
+                <?php endif; ?>
+                <form action="<?= base_url('Auth'); ?>" method="post">
+                <div> 
+                    <div >
+                        <label>Username</label>
+                        <input class="nama" type="text" placeholder="Username" name="username" required/>
+                        <?= form_error('username', '<small class="text-danger pl-3">','</small>'); ?>
+                    </div>
+                    <div >
+                        <label>Password</label>
+                        <input class="nama" type="password" placeholder="password" name="password" required/>
+                        <?=  form_error('password', '<large class="text-danger pl-4">','</large>'); ?>
+                    </div>
+                </div>
+                <input class="kirim" type="submit" value="Kirim" name="login">
         </div>
-        <div>
-        <label>Password:</label>
-        <input type="password" placeholder="password" name="password" required/>
-        <?=  form_error('password', '<large class="text-danger pl-4">','</large>'); ?>
-        </div>
-        <input type="submit" value="Kirim" name="login">
-    </form>
+
+    </div>        
+        </form>
 </body>
 </html>
